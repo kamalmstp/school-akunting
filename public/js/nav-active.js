423597162c38dbfc39261a0192bd090a319268c8
Nuremax <mustapakamalkml@gmail.com>
2025-09-29 19:43:49 +0800

    jbhksfdsf

--- public/js/nav-active.js
+++ public/js/nav-active.js
@@ -1,112 +1,115 @@
-$(document).ready(function() {
-    let url = window.location.href;
-
-    $('.treeview').removeClass('active current-page');
-    $('.treeview > ul.treeview-menu').removeClass('menu-open');
-    $('.treeview-menu li a').removeClass('active-sub');
-
-    if (url.includes('teachers') || url.includes('employees') || url.includes('school-majors') || url.includes('students') || url.includes('fund-managements') || url.includes('financial-periods') || url.includes('cash-managements')) {
-        $('.master').addClass('active current-page');
-        $('.master > ul.treeview-menu').addClass('menu-open');
-    }
-
-    if (url.includes('teachers') && !url.includes('receivables')) {
-        $('.m-first').addClass('active-sub');
-    }
-    if (url.includes('employees') && !url.includes('receivables')) {
-        $('.m-second').addClass('active-sub');
-    }
-    if (url.includes('school-majors')) {
-        $('.m-third').addClass('active-sub');
-    }
-    if (url.includes('students') && !url.includes('receivables') && !url.includes('alumni') && !url.includes('receipts')) {
-        $('.m-fourth').addClass('active-sub');
-    }
-    if (url.includes('fund-managements') || url.includes('cash-managements')) {
-        $('.m-fifth').addClass('active-sub');
-    }
-    if (url.includes('financial-periods')) {
-        $('.m-sixth').addClass('active-sub');
-    }
-
-    if (url.includes('receivables') || url.includes('receipts')) {
-        $('.receivable').addClass('active current-page');
-        $('.receivable > ul.treeview-menu').addClass('menu-open');
-    }
-
-    if (url.includes('student-receivables')) {
-        $('.p-first').addClass('active-sub');
-    }
-    if (url.includes('teacher-receivables')) {
-        $('.p-second').addClass('active-sub');
-    }
-    if (url.includes('employee-receivables')) {
-        $('.p-third').addClass('active-sub');
-    }
-    if (url.includes('student-receipts') || url.includes('receipts/filter')) {
-        $('.p-fourth').addClass('active-sub');
-    }
-
-    if (url.includes('alumni')) {
-        $('.alumni').addClass('active current-page');
-        $('.alumni > ul.treeview-menu').addClass('menu-open');
-        $('.a-first').addClass('active-sub');
-    }
-
-    if (url.includes('transactions/index') || url.includes('fixed-assets')) {
+$(document).ready(function () {
+    const url = window.location.href;
+    if (url.includes('transactions')) {
         $('.transaction').addClass('active current-page');
-        $('.transaction > ul.treeview-menu').addClass('menu-open');
-    }
-    
-    if (url.includes('transactions/index') && !url.includes('reports')) {
+        $('.transaction > ul').addClass('menu-open');
         $('.tr-first').addClass('active-sub');
     }
     if (url.includes('fixed-assets')) {
+        $('.transaction').addClass('active current-page');
+        $('.transaction > ul').addClass('menu-open');
         $('.tr-second').addClass('active-sub');
     }
-
-    if (url.includes('/reports/')) {
+    if (url.includes('beginning-balance')) {
         $('.report').addClass('active current-page');
-        $('.report > ul.treeview-menu').addClass('menu-open');
-    }
-    
-    if (url.includes('reports/beginning-balance')) {
+        $('.report > ul').addClass('menu-open');
         $('.rp-first').addClass('active-sub');
     }
-    if (url.includes('reports/general-journal')) {
+    if (url.includes('general-journal')) {
+        $('.report').addClass('active current-page');
+        $('.report > ul').addClass('menu-open');
         $('.rp-second').addClass('active-sub');
     }
-    if (url.includes('reports/ledger')) {
+    if (url.includes('ledger')) {
+        $('.report').addClass('active current-page');
+        $('.report > ul').addClass('menu-open');
         $('.rp-third').addClass('active-sub');
     }
-    if (url.includes('reports/trial-balance-before')) {
+    if (url.includes('trial-balance-before')) {
+        $('.report').addClass('active current-page');
+        $('.report > ul').addClass('menu-open');
         $('.rp-fourth').addClass('active-sub');
     }
-    if (url.includes('reports/adjusting-entries')) {
+    if (url.includes('adjusting-entries')) {
+        $('.report').addClass('active current-page');
+        $('.report > ul').addClass('menu-open');
         $('.rp-fifth').addClass('active-sub');
     }
-    if (url.includes('reports/trial-balance-after')) {
+    if (url.includes('trial-balance-after')) {
+        $('.report').addClass('active current-page');
+        $('.report > ul').addClass('menu-open');
         $('.rp-sixth').addClass('active-sub');
     }
-    if (url.includes('reports/financial-statements')) {
+    if (url.includes('financial-statements')) {
+        $('.report').addClass('active current-page');
+        $('.report > ul').addClass('menu-open');
         $('.rp-seventh').addClass('active-sub');
     }
-    if (url.includes('reports/cash-reports')) {
+    if (url.includes('cash-reports')) {
+        $('.report').addClass('active current-page');
+        $('.report > ul').addClass('menu-open');
         $('.rp-eighth').addClass('active-sub');
     }
-    if (url.includes('reports/rkas-global')) {
+
+    if (url.includes('rkas')) {
+        $('.report').addClass('active current-page');
+        $('.report > ul').addClass('menu-open');
         $('.rp-nineth').addClass('active-sub');
     }
+    if (url.includes('teachers')) {
+        $('.master').addClass('active current-page');
+        $('.master > ul').addClass('menu-open');
+        $('.m-first').addClass('active-sub');
+    }
+    if (url.includes('employees')) {
+        $('.master').addClass('active current-page');
+        $('.master > ul').addClass('menu-open');
+        $('.m-second').addClass('active-sub');
+    }
+    if (url.includes('majors')) {
+        $('.master').addClass('active current-page');
+        $('.master > ul').addClass('menu-open');
+        $('.m-third').addClass('active-sub');
+    }
+    if (url.includes('students')) {
+        $('.master').addClass('active current-page');
+        $('.master > ul').addClass('menu-open');
+        $('.m-fourth').addClass('active-sub');
+    }
+    if (url.includes('cash-managements')) {
+        $('.master').addClass('active current-page');
+        $('.master > ul').addClass('menu-open');
+        $('.m-fifth').addClass('active-sub');
+    }
+    if (url.includes('financial-periods')) {
+        $('.master').addClass('active current-page');
+        $('.master > ul').addClass('menu-open');
+        $('.m-sixth').addClass('active-sub');
+    }
     
-    if (url.includes('/accounts') && !url.includes('school-accounts')) {
-         $('.sidebar-menu > li:has(a[href*="/accounts"])').addClass('active current-page');
+    if (url.includes('student-receivables')) {
+        $('.receivable').addClass('active current-page');
+        $('.receivable > ul').addClass('menu-open');
+        $('.p-first').addClass('active-sub');
     }
-    if (url.includes('school-accounts')) {
-         $('.sidebar-menu > li:has(a[href*="school-accounts"])').addClass('active current-page');
+    if (url.includes('teacher-receivables')) {
+        $('.receivable').addClass('active current-page');
+        $('.receivable > ul').addClass('menu-open');
+        $('.p-second').addClass('active-sub');
     }
-
-    if (url.endsWith('/dashboard') || url.endsWith('/schools')) {
-        $('.sidebar-menu > li:has(a[href$="/dashboard"])').addClass('active current-page');
-        $('.sidebar-menu > li:has(a[href$="/schools"])').addClass('active current-page');
+    if (url.includes('employee-receivables')) {
+        $('.receivable').addClass('active current-page');
+        $('.receivable > ul').addClass('menu-open');
+        $('.p-third').addClass('active-sub');
     }
-});
+    if (url.includes('student-receipts')) {
+        $('.receivable').addClass('active current-page');
+        $('.receivable > ul').addClass('menu-open');
+        $('.p-fourth').addClass('active-sub');
+    }
+    if (url.includes('student-alumni')) {
+        $('.alumni').addClass('active current-page');
+        $('.alumni > ul').addClass('menu-open');
+        $('.a-first').addClass('active-sub');
+    }
+})
